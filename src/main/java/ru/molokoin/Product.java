29cf8a86ef1fd42f170c313f675d0b03abb0cfb3
esoe <igor_nikulin@mail.ru>
2023-02-22 13:26:20 +0300

    qq

---
+++ src/main/java/ru/molokoin/Product.java
@@ -0,0 +1,8 @@
+package main.java.ru.molokoin;
+
+/**
+ * Продукт возникает у поставщика в случайном количестве
+ */
+public class Product {
+    
+}
---
+++ src/main/java/ru/molokoin/Supplier.java
@@ -0,0 +1,5 @@
+package main.java.ru.molokoin;
+
+public class Supplier extends Thread{
+    
+}
